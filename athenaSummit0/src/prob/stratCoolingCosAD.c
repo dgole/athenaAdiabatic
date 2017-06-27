4cf6532c0a77692157b9110ff2257a7790f65f86
Dan Gole <dagl4841@ccube.colorado.edu>
2017-06-27 16:12:55 -0600

    stop printing diagnostic stuff

--- athenaSummit0/src/prob/stratCoolingCosAD.c
+++ athenaSummit0/src/prob/stratCoolingCosAD.c
@@ -1116,7 +1116,6 @@ void Userwork_in_loop(MeshS *pM)
   }}}
 
 #ifdef RESISTIVITY
-	printf("%s %i \n", "gridnumx1 is ", gridnumx1);
   /* user defined diffusivities from look-up tables */
   if (par_geti_def("problem","CASE",1) == 3) { /* requires CASE = 3 */
 		count1 = count1 + 1;
