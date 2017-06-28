4ce69d8304e945eb548de692de1e7f020fad7480
Dan Gole <dagl4841@ccube.colorado.edu>
2017-06-28 11:09:45 -0600

    cleaned up print out statements

--- athenaSummit0/src/prob/stratCoolingCosAD.c
+++ athenaSummit0/src/prob/stratCoolingCosAD.c
@@ -1127,19 +1127,19 @@ void Userwork_in_loop(MeshS *pM)
 			density_profile(pM);
 			diff1 = clock() - start1;
 			int msec1 = diff1 * 1000 / CLOCKS_PER_SEC;
-			printf("density_profile Time taken %d seconds %d milliseconds", msec1/1000, msec1%1000);
+			printf("density_profile Time taken %d seconds %d milliseconds \n", msec1/1000, msec1%1000);
 
 			clock_t start2 = clock(), diff2;
 			combine_densities(pM);
 			diff2 = clock() - start2;
 			int msec2 = diff2 * 1000 / CLOCKS_PER_SEC;
-			printf("density_profile Time taken %d seconds %d milliseconds", msec2/1000, msec2%1000);
+			printf("density_profile Time taken %d seconds %d milliseconds \n", msec2/1000, msec2%1000);
 
 			clock_t start3 = clock(), diff3;
 			combine_densities(pM);
 			diff3 = clock() - start3;
 			int msec3 = diff3 * 1000 / CLOCKS_PER_SEC;
-			printf("density_profile Time taken %d seconds %d milliseconds", msec3/1000, msec3%1000);
+			printf("density_profile Time taken %d seconds %d milliseconds \n", msec3/1000, msec3%1000);
 
     	//density_profile(pM);
     	//ionization_rate(pM);
