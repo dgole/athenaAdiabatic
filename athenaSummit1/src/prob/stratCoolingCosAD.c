1457e3f0701aeffc483d1c6b1a6ff64e297b8d17
Dan Gole <dagl4841@ccube.colorado.edu>
2017-09-18 11:28:13 -0600

    working on get_Am_FUV

--- athenaSummit1/src/prob/stratCoolingCosAD.c
+++ athenaSummit1/src/prob/stratCoolingCosAD.c
@@ -898,7 +898,7 @@ static Real get_Am_FUV(const GridS *pG, const int i, const int j, const int k, c
     Am = Am_FUV*pG->U[k][j][i].d;
   }
 */
-  if (i==0 && j==0) {printf("%s %i %i %0.9G %0.9G %0.9G %0.9G %0.9G %0.9G %i %i %0.9G \n", "get_Am_FUV", myID_Comm_world, k, z, Am, zib, zit, delz, ndelz, kbase1, kbase2, ionfrac_FUV);}
+  if (i==0 && j==0) {printf("%s %i %i %i %0.9G %0.9G %0.9G %0.9G %0.9G %0.9G %i %i %0.9G \n", "get_Am_FUV", myID_Comm_world, k, kg, z, Am, zib, zit, delz, ndelz, kbase1, kbase2, ionfrac_FUV);}
 	//Am = 1000.0;
   return Am;
 }
